188c7a1fbcd2d8ca00752b5e3919cf83524e9144
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2023-11-09 17:20:51 +0530

    Time: 25 ms (63.26%) | Memory: 12 MB (53.48%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    int MOD=1e9+7;
+   
     int countHomogenous(string s) {
-        
+         int MOD=1e9+7;
         int n=s.length();
         int streak=0;
       int result=0;
         for(int i=0;i<n;i++){
+//              i>0 ei condition ta na dile runtime error dicche
             if( i>0 && s[i]==s[i-1]){  // same mal
                 streak+=1;
             }else{
